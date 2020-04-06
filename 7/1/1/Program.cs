289c45bb0297366db3429b1c2d3195d90124366c
MaxxVaida <57134380+MaxxVaida@users.noreply.github.com>
2020-04-07 01:01:23 +0300

    Add files via upload

---
+++ 7/1/1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            Console.Write("Кількість рядків = ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Кількість стовпців = ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            double[,] a = new double[rows, columns];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write($"a[{i}][{j}] = ");
+                    a[i, j] = Convert.ToDouble(Console.ReadLine());
+                    if (a[i, j] > 0 && i % 2 == 0 && j % 2 == 1)
+                        sum += a[i, j];
+                }
+            }
+            Console.WriteLine($"Сума = {sum}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 7/2/2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Матриця A
+            Console.Write("Кількість рядків A = ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Кількість стовпців A = ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            double[,] a = new double[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"a[{i}][{j}] = ");
+                    a[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            //// Матриця B
+            Console.WriteLine("Введіть матрицю B");
+            double[,] b = new double[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"b[{i}][{j}] = ");
+                    b[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            //// Розвязок
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (a[i, j] == 0)
+                        a[i, j] = b[i, j];
+                }
+            }
+            Console.WriteLine("Відповідь:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine($"a[{i}][{j}]={a[i, j]}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
