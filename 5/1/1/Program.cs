acd944fc586f1f6a900ecfa83b64d752cf3396c4
MaxxVaida <57134380+MaxxVaida@users.noreply.github.com>
2020-04-06 23:58:36 +0300

    Add files via upload

---
+++ 5/1/1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s = 0;
+            Console.Write("Hello, a=");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("ok, n=");
+            double n = Convert.ToDouble(Console.ReadLine());
+            for (; n > 0; n--)
+            {
+                s += Math.Log(Math.Pow(a, n));
+                n -= 1;
+            }
+            Console.WriteLine($"Сума = {s}");
+        }
+    }
+}
---
+++ 5/2/2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.Write("Введіть число: ");
+            string num = Convert.ToString(Console.ReadLine());
+            foreach (var i in num)
+            {
+                if (i == '0')
+                    count += 1;
+            }
+            Console.WriteLine($"Кількість нулів - {count}");
+        }
+    }
+}
